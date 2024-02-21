9edcb5268363bce1c7e5e6c484f6a4c6f3db54b4
nicolasvanermen <nicolas.vanermen@inbo.be>
2024-02-21 17:10:42 +0100

    Update Broed_all revised 2024.R

--- Broed_all revised 2024.R
+++ Broed_all revised 2024.R
@@ -1,7 +1,5 @@
 setwd("C:/Users/nicolas_vanermen/Desktop/Hilbran")
 
-# install.packages(c("checklist", "INBOmd"))
-
 library(tidyverse)
 library(leaflet)
 library(sf)
@@ -23,12 +21,8 @@ W <- W %>%
   mutate(UTC_datetime = ymd_hms(UTC_datetime)) %>%
   filter(!is.na(UTC_datetime))
 
-
 W$Latitude <- as.numeric(W$Latitude)
 W$Longitude <- as.numeric(W$Longitude)
-
-W$month <- month(W$UTC_datetime)
-W$year <- year(W$UTC_datetime)
 
 # Vliegende beesten eruit
 W <- W[W$speed_km_h < 10,]
@@ -55,25 +49,24 @@ ggplot(W_30min[W_30min$diff_sec<2500,], aes(diff_sec)) + geom_histogram() + scal
 
 #zonsopgang/zonsondergang
 efemeriden <- getSunlightTimes(date = seq.Date(as.Date("2020-01-01"), as.Date("2023-12-31"), by = 1),
-                               keep = c("sunrise", "sunset"), lat=50.90, lon=5.12,tz="UTC")
+                               keep = c("sunrise", "sunset"), lat=50.90, lon=5.12, tz="UTC")
 head(efemeriden)
 efemeriden$date.chr <- as.character(efemeriden$date)
 
 W_30min <- merge(W_30min, efemeriden[,c("date.chr","sunrise","sunset")], by.x="UTC_date", by.y="date.chr")
 
-#"day" definieren als alles tussen 1 uur voor zonsopgang en 1 uur na zonsondergang
-#hier stond een foutje denk ik en doe nu sunset + 3600 ipv sunset - 3600
-W_30min$day <- ifelse(W_30min$UTC_datetime > (W_30min$sunrise-3600) &
+#"dag" definieren als alles tussen 1 uur voor zonsopgang en 1 uur na zonsondergang
+W_30min$dag <- ifelse(W_30min$UTC_datetime > (W_30min$sunrise-3600) &
                       W_30min$UTC_datetime < (W_30min$sunset+3600), TRUE, FALSE)
-summary(W_30min$day)
+summary(W_30min$dag)
 
 #KERNEL LOOP
-table(W$Naam,W$year)
-W_30min$Name_year <- paste(W_30min$Naam, W_30min$year)
-Name_year <- unique(W_30min$Name_year)
-Name_year
+table(W$Naam,W$jaar)
+W_30min$Naam_jaar <- paste(W_30min$Naam, W_30min$jaar)
+Naam_jaar <- unique(W_30min$Naam_jaar)
+Naam_jaar
 
-BS_data <- matrix(nrow=17, ncol=2, dimnames=list(Name_year,c("Start_BS","End_BS")))
+BS_data <- matrix(nrow=17, ncol=2, dimnames=list(Naam_jaar,c("Start_BS","End_BS")))
 BS_data["Herlaar 2020",] <- c("2020-05-01","2020-06-01")
 BS_data["Henk-Jan 2020",] <- c("2020-05-06","2020-06-09")
 BS_data["Peter 2020",] <- c("2020-05-28","2020-07-05")
@@ -94,7 +87,7 @@ BS_data["Jorg 2023",] <- c("2023-05-01","2023-06-01")
 BS_data
 
 BS_data <- as.data.frame(BS_data)
-BS_data$Name_year <- rownames(BS_data)
+BS_data$Naam_jaar <- rownames(BS_data)
 BS_data
 
 W_30min <- merge(W_30min, BS_data)
@@ -104,21 +97,21 @@ pal <- colorNumeric(c("orangered4","orangered"), 1:2)
 pal(1:2)
 
 #select dag of nacht (indien enkel dagwaarden relevant: bovenste lijn aktief zetten, indien nachtawaarden relevant: tweede lijn aktief zetten)
-W_30min_loop <- W_30min[W_30min$day==TRUE,]
-# W_30min_loop <- W_30min[W_30min$day==FALSE,]
+W_30min_loop <- W_30min[W_30min$dag==TRUE,]
+# W_30min_loop <- W_30min[W_30min$dag==FALSE,]
 # W_30min_loop <- W_30min
 
 #er loopt hier iets mis met de coördinaten, zowel latitude als longitude zijn 0:
 summary(W_30min_loop[,c("Longitude", "Latitude")])
 W_30min_loop[W_30min_loop$Latitude==0, c("Longitude", "Latitude")]
-# ggplot(W_30min_BS, aes(Longitude, Latitude)) + geom_point()
-# ggplot(W_30min_loop, aes(Longitude, Latitude)) + geom_point()
 #deze waarnemingen verwijderen!
 W_30min_loop <- W_30min_loop[W_30min_loop$Latitude != 0,]
 
-for (i in Name_year)
+setwd("C:/Users/nicolas_vanermen/Desktop/Hilbran/Export")
+
+for (i in Naam_jaar)
 {
-  W_i <- W_30min_loop %>% filter(Name_year==i)
+  W_i <- W_30min_loop %>% filter(Naam_jaar==i)
   W_i <- W_i %>% filter(ymd(UTC_date) < ymd(End_BS) & ymd(UTC_date) > ymd(Start_BS))
 
   coordinates(W_i) <- c("Longitude", "Latitude")
@@ -171,8 +164,6 @@ for (i in unique(W_30min_loop$Naam))
     leaflet() %>%
     addTiles(group = "OSM") %>%
     addCircles(lng = W_i$Longitude, lat = W_i$Latitude, color = ~factpal(W_i$jaar),radius=10) %>%
-    # addPolygons(data = HR_50, fill = TRUE, color = ~pal(1), weight = 2) %>%
-    # addPolygons(data = HR_95, fill = TRUE, color = ~pal(2), weight = 2) %>%
     addLegend(pal = factpal, values = ~jaar, opacity = 1)
   
   W_i_map
@@ -184,20 +175,5 @@ for (i in unique(W_30min_loop$Naam))
   webshot(filename_html, file = filename_png, cliprect = "viewport")
 }
 
-Broed_zit_dag <- W_30min_loop %>% filter(ymd(UTC_date) < ymd(End_BS) & ymd(UTC_date) > ymd(Start_BS))
-write.csv2(Broed_zit_dag, "Broed_zit_dag.csv", row.names = FALSE)
-
-# #test vliegend/zittend
-# library(ggplot2)
-# ggplot(W, aes(speed_km_h)) + geom_histogram()
-# ggplot(W, aes(month)) + geom_bar(stat="count")
-# ggplot(W[W$month %in% c(2),], aes(speed_km_h)) + geom_histogram()
-# ggplot(W[W$month %in% c(5),], aes(speed_km_h)) + geom_histogram()
-# 
-# sum(W$speed_km_h>10)/length(W$speed_km_h)
-# sum(W$speed_km_h>5)/length(W$speed_km_h)
-# #2.3% met snelheid hoger dan 10 km/h
-# #3.2% met snelheid hoger dan 5 km/h
-# 
-# #een selectie op snelheid zal vermoedelijk heel weinig veranderen in de kernel resultaten
-
+Wulp_BS_niet_vliegend <- W_30min_loop %>% filter(ymd(UTC_date) < ymd(End_BS) & ymd(UTC_date) > ymd(Start_BS))
+write.csv2(Wulp_BS_niet_vliegend, "Wulp_BS_niet_vliegend.csv", row.names = FALSE)
